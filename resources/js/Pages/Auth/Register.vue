4cf2f483be9eac892a9ab791f2757af9a96fcd9f
Adam Suchi Hafizullah <adamsuchihafizullah@gmail.com>
2024-10-04 20:48:04 +0700

    update: user register handler #6

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -4,6 +4,21 @@ import Title from '../../Components/Title.vue';
 import TextLink from '../../Components/TextLink.vue';
 import InputField from '../../Components/InputField.vue';
 import PrimaryBtn from '../../Components/PrimaryBtn.vue';
+import ErrorMessages from '../../Components/ErrorMessages.vue';
+import { useForm } from '@inertiajs/vue3';
+
+const form = useForm({
+    name: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
+})
+
+const submit = () => {
+    form.post(route("register"), {
+        onFinish: () => form.reset("password", "password_confirmation")
+    })
+}
 </script>
 
 <template>
@@ -15,17 +30,18 @@ import PrimaryBtn from '../../Components/PrimaryBtn.vue';
             </p>
         </div>
 
-        <form class="space-y-6">
-            <InputField label="Name" icon="id-badge" />
-            <InputField label="Email" icon="at" type="email" />
-            <InputField label="Password" icon="key" type="password" />
-            <InputField label="Confirm Password" icon="key" type="password" />
+        <ErrorMessages :errors="form.errors"/>
+        <form @submit.prevent="submit" class="space-y-6">
+            <InputField label="Name" icon="id-badge" v-model="form.name" />
+            <InputField label="Email" icon="at" v-model="form.email" />
+            <InputField label="Password" icon="key" type="password" v-model="form.password" />
+            <InputField label="Confirm Password" icon="key" type="password" v-model="form.password_confirmation" />
 
             <p class="text-slate-500 text-sm dark:text-slate-400">
                 By creating an account, you agree to our Terms of Service and Privacy Policy
             </p>
 
-            <PrimaryBtn>Register</PrimaryBtn>
+            <PrimaryBtn :disabled="form.processing">Register</PrimaryBtn>
         </form>
     </Container>
 </template>
