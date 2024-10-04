e5aea93a41d403979e832d9f362c466cb26c9500
Adam Suchi Hafizullah <adamsuchihafizullah@gmail.com>
2024-10-05 05:55:25 +0700

    update: user login form #7

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -5,44 +5,43 @@ import TextLink from '../../Components/TextLink.vue';
 import InputField from '../../Components/InputField.vue';
 import PrimaryBtn from '../../Components/PrimaryBtn.vue';
 import ErrorMessages from '../../Components/ErrorMessages.vue';
+import CheckBox from '../../Components/CheckBox.vue';
 import { Head, useForm } from '@inertiajs/vue3';
 
 const form = useForm({
-    name: "",
     email: "",
     password: "",
-    password_confirmation: "",
+    remember: null,
 })
 
 const submit = () => {
-    form.post(route("register"), {
-        onFinish: () => form.reset("password", "password_confirmation")
+    form.post(route("login"), {
+        onFinish: () => form.reset("password")
     })
 }
 </script>
 
 <template>
-    <Head title="- Register"/>
+    <Head title="- Login"/>
     <Container class="w-1/2">
         <div class="mb-8 text-center">
-            <Title>Register a new account</Title>
-            <p>Already have an account?
-                <TextLink routeName="login" label="Login" />
+            <Title>Login to your account</Title>
+            <p>Need an account?
+                <TextLink routeName="register" label="Register" />
             </p>
         </div>
 
         <ErrorMessages :errors="form.errors"/>
         <form @submit.prevent="submit" class="space-y-6">
-            <InputField label="Name" icon="id-badge" v-model="form.name" />
             <InputField label="Email" icon="at" v-model="form.email" />
             <InputField label="Password" icon="key" type="password" v-model="form.password" />
-            <InputField label="Confirm Password" icon="key" type="password" v-model="form.password_confirmation" />
 
-            <p class="text-slate-500 text-sm dark:text-slate-400">
-                By creating an account, you agree to our Terms of Service and Privacy Policy
-            </p>
+            <div class="flex items-center justify-between">
+                <CheckBox name="remember" v-model="form.remember">Remember me</CheckBox>
+                <TextLink routeName="home" label="Forgot password?"/>
+            </div>
 
-            <PrimaryBtn :disabled="form.processing">Register</PrimaryBtn>
+            <PrimaryBtn :disabled="form.processing">Login</PrimaryBtn>
         </form>
     </Container>
 </template>
