9bb20735016830592e1d75136ea1e5059077121f
Adam Suchi Hafizullah <adamsuchihafizullah@gmail.com>
2024-10-06 11:39:46 +0700

    update: user logout handler #9

--- resources/js/Layouts/Main.vue
+++ resources/js/Layouts/Main.vue
@@ -1,16 +1,37 @@
 <script setup>
 import { switchTheme } from '../theme';
 import NavLink from '../Components/NavLink.vue';
+import { Link, usePage } from '@inertiajs/vue3';
+import { computed, ref } from 'vue';
+
+const page = usePage();
+const user = computed(() => page.props.auth.user);
+
+const show = ref(false)
 </script>
 
 <template>
+    <!-- overlay -->
+    <div v-show="show" @click="show = false" class="fixed inset-0 z-40"></div>
     <header class="bg-slate-800 text-white">
         <nav class="p-6 mx-auto max-w-screen-lg flex items-center justify-between">
             <NavLink routeName="home" componentName="Home">Home</NavLink>
 
             <div class="flex items-center space-x-6">
-                <NavLink routeName="login" componentName="Auth/Login">Login</NavLink>
-                <NavLink routeName="register" componentName="Auth/Register">Register</NavLink>
+                <div v-if="user" class="relative">
+                    <div class="flex items-center gap-2 px-3 py-1 rounded-lg hover:bg-slate-700 cursor-pointer" @click="show = !show" :class="{'bg-slate-700' : show}">
+                        <p>{{ user.name }}</p>
+                        <i class="fa-solid fa-angle-down"></i>
+                    </div>
+                    <div class="absolute z-50 top-16 right-0 bg-slate-800 text-white rounded-lg border-slate-300 border overflow-hidden w-40" v-show="show" @click="show=false">
+                        <Link class="block w-full px-6 py-3 hover:bg-slate-700 text-left">Dashboard</Link>
+                        <Link :href="route('logout')" method="post" as="button" class="block w-full px-6 py-3 hover:bg-slate-700 text-left">Logout</Link>
+                    </div>
+                </div>
+                <div v-else class="space-x-6">
+                    <NavLink routeName="login" componentName="Auth/Login">Login</NavLink>
+                    <NavLink routeName="register" componentName="Auth/Register">Register</NavLink>
+                </div>
                 <button
                     @click="switchTheme"
                     class="hover:bg-slate-700 w-6 h-6 grid place-items-center rounded-full hover:outline outline-1 outline-white">
