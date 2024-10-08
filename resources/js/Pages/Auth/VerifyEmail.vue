6964146109f4d812a218a9824e5468cab33fcd9b
Adam Suchi Hafizullah <adamsuchihafizullah@gmail.com>
2024-10-08 20:58:06 +0700

    update: email verification #10

--- resources/js/Pages/Auth/VerifyEmail.vue
+++ resources/js/Pages/Auth/VerifyEmail.vue
@@ -1,4 +1,21 @@
-<script setup></script>
+<script setup>
+import { useForm } from '@inertiajs/vue3';
+import Container from '../../Components/Container.vue'
+import PrimaryBtn from '../../Components/PrimaryBtn.vue'
+import SessionMessages from '../../Components/SessionMessages.vue'
+
+const form = useForm({
+
+})
+
+defineProps({
+    status: String,
+})
+
+const submit = () => {
+    form.post(route('verification.send'))
+}
+</script>
 <template>
     <Head title="- Email Verification"></Head>
     <Container class="w-1/2">
@@ -7,5 +24,9 @@
                 Thanks for signing up! Before getting started, could you verify your email address by clicking one the link we just emailed to you? If you didn't receive the email, we will gladly send you another.
             </p>
         </div>
+        <SessionMessages :status="status"/>
+        <form @submit.prevent="submit">
+            <PrimaryBtn :disabled="form.processing">Resend Verification Email</PrimaryBtn>
+        </form>
     </Container>
 </template>
--- routes/auth.php
+++ routes/auth.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Auth\AuthenticateController;
 use App\Http\Controllers\Auth\EmailVerificationController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ResetPasswordController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function(){
@@ -11,10 +12,16 @@ Route::middleware('guest')->group(function(){
 
     Route::get('/login', [AuthenticateController::class, 'create'])->name('login');
     Route::post('/login', [AuthenticateController::class, 'store']);
+
+    Route::get('/forgot-password', [ResetPasswordController::class, 'requestPass'])->name('password.request');
 });
 
 Route::middleware('auth')->group(function(){
     Route::post('/logout', [AuthenticateController::class, 'destroy'])->name('logout');
 
-    Route::get('/email/verify', [EmailVerificationController::class, 'notice'])->middleware('auth')->name('verification.notice');
+    Route::get('/email/verify', [EmailVerificationController::class, 'notice'])->name('verification.notice');
+
+    Route::get('/email/verify/{id}/{hash}', [EmailVerificationController::class, 'handler'])->middleware('signed')->name('verification.verify');
+
+    Route::post('/email/verification-notification', [EmailVerificationController::class, 'resend'])->middleware('throttle:6,1')->name('verification.send');
 });
