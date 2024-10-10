0b35acec115efdb00bbab8cf9ac9e4866979f098
Adam Suchi Hafizullah <adamsuchihafizullah@gmail.com>
2024-10-10 21:56:35 +0700

    update: update user info #14

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -1,4 +1,13 @@
-<script setup></script>
+<script setup>
+import UpdateInfo from './Sections/UpdateInfo.vue';
+
+defineProps({
+    user: Object,
+    status: String
+})
+</script>
+
 <template>
-    <div>Profile</div>
+    <Head title="- Profile"></Head>
+    <UpdateInfo :user="user" :status="status"/>
 </template>
