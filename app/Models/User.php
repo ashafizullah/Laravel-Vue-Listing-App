495aea1201b3ec1446f7abb64c47098a41634e85
Adam Suchi Hafizullah <adamsuchihafizullah@gmail.com>
2024-10-18 19:27:07 +0700

    update: listing model preparation #16

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,8 @@ class User extends Authenticatable implements MustVerifyEmail
             'password' => 'hashed',
         ];
     }
+
+    public function listings(){
+        return $this->hasMany(Listing::class);
+    }
 }
