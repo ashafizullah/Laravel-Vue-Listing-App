944efd4a783d0f3ced4895768f10ba0afef15db3
Adam Suchi Hafizullah <adamsuchihafizullah@gmail.com>
2024-11-11 22:53:41 +0700

    update: create a listing form #23

--- resources/js/Pages/Listing/Create.vue
+++ resources/js/Pages/Listing/Create.vue
@@ -5,6 +5,7 @@ import InputField from '../../Components/InputField.vue';
 import ErrorMessage from '../../Components/ErrorMessages.vue';
 import PrimaryBtn from '../../Components/PrimaryBtn.vue';
 import TextArea from '../../Components/TextArea.vue';
+import ImageUpload from '../../Components/ImageUpload.vue';
 import { useForm } from '@inertiajs/vue3';
 
 const form = useForm({
@@ -18,20 +19,48 @@ const form = useForm({
 </script>
 
 <template>
+
     <Head title="- New Listing"></Head>
     <Container>
         <div class="mb-6">
             <Title>Create a New Listing</Title>
         </div>
+        <ErrorMessage :errors="form.errors" />
         <form class="grid grid-cols-2 gap-6">
             <div class="space-y-6">
-                <InputField label="Title" icon="heading" placeholder="Title" v-model="form.title" />
-                <InputField label="Tags (separate with comma)" icon="tags" placeholder="one, two, three" v-model="form.tags" />
-                <TextArea label="Description" icon="newspaper" placeholder="https://example.com" v-model="form.link"></TextArea>
+                <InputField
+                    label="Title"
+                    icon="heading"
+                    placeholder="Title"
+                    v-model="form.title"
+                />
+                <InputField
+                    label="Tags (separate with comma)"
+                    icon="tags"
+                    placeholder="one, two, three"
+                    v-model="form.tags"
+                />
+                <TextArea
+                    label="Description"
+                    icon="newspaper"
+                    placeholder="https://example.com"
+                    v-model="form.link"
+                ></TextArea>
             </div>
             <div class="space-y-6">
-                <InputField label="Email" icon="at" placeholder="example@email.com" v-model="form.email" />
-                <InputField label="External Link" icon="up-right-from-square" placeholder="https://example.com" v-model="form.link" />
+                <InputField
+                    label="Email"
+                    icon="at"
+                    placeholder="example@email.com"
+                    v-model="form.email"
+                />
+                <InputField
+                    label="External Link"
+                    icon="up-right-from-square"
+                    placeholder="https://example.com"
+                    v-model="form.link"
+                />
+                <ImageUpload @image="(e) => (form.images = e)" />
             </div>
             <div>
                 <PrimaryBtn>Create</PrimaryBtn>
